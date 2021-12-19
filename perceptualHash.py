66b36ac7336349fb4ac556b4324606e0779257db
Hiroshi Nakayama <hrs.nkym@gmail.com>
2021-12-19 21:45:17 +0900

    fix: change pass, imagehash.*

--- perceptualHash.py
+++ perceptualHash.py
@@ -7,21 +7,28 @@ import scipy
 import scipy.fftpack
 
 
-TARGET_FILE = '/Users/odyss/Documents/Leafhub/work/clairvision/imageHash/pict/01/IMG_0023_BURST00120211210142515.JPG'
-IMG_DIR = '/Users/odyss/Documents/Leafhub/work/clairvision/imageHash/pict/'
+TARGET_FILE = 'IMG_0023_BURST00120211210142554.JPG'
+IMG_DIR = './pict/'
 target_img_path = IMG_DIR + TARGET_FILE
-target_hash = imagehash.average_hash(Image.open(target_img_path))
+# target_hash = imagehash.average_hash(Image.open(target_img_path))
+target_hash = imagehash.phash(Image.open(target_img_path))
+# target_hash = imagehash.dhash(Image.open(target_img_path))
+# target_hash = imagehash.whash(Image.open(target_img_path))
 files = os.listdir(IMG_DIR)
 
 print('TARGET_FILE: %s' % (TARGET_FILE), target_hash)
 print('files: %s' % (files))
+
 for file in files:
-    if file == '.DS_Store' or file == TARGET_FILE:
+    if file == TARGET_FILE:
         continue
 
     comparing_img_path = IMG_DIR + file
     try:
-        hash = imagehash.average_hash(Image.open(comparing_img_path))
+        # hash = imagehash.average_hash(Image.open(comparing_img_path))
+        hash = imagehash.phash(Image.open(comparing_img_path))
+        # hash = imagehash.dhash(Image.open(comparing_img_path))
+        # hash = imagehash.whash(Image.open(comparing_img_path))
         haming = target_hash - hash
     except cv2.error:
         ret = 100000
